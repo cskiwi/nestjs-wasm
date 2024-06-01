3e2e82083574bc4ae68b28c0877168b858a3e7a1
cskiwi <glenn.latomme@gmail.com>
2024-06-01 19:45:53 +0200

    close but not yet

--- libs/backend/test/src/lib/backend-test.service.ts
+++ libs/backend/test/src/lib/backend-test.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { sum } from '@app/rust-testing';
 
 @Injectable()
 export class BackendTestService {
   getHello(): string {
     return 'Hello you!';
+  }
+
+  getGoodbye() {
+    return {
+      sum: sum(1, 2),
+    };
   }
 }
